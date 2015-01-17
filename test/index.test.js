a751f46610e82d7342df5d7999e5564cffbd7a7c
camshaft <bytheway.cameron@gmail.com>
2015-01-17 08:33:26 -0700

    add unbuffered support

--- test/index.test.js
+++ test/index.test.js
@@ -9,7 +9,10 @@ var ast2template = require('ast2template');
 describe('rework2ast', function() {
   it('should work', function() {
     var ast = rework2ast.file(__dirname + '/cases/index.styl');
-    var str = ast2template(ast);
+    console.log(ast)
+    var str = ast2template(ast, {
+      keyName: false
+    });
     console.log(str);
   });
 });
